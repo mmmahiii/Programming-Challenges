675ca35146c3e2a6cc6860b2420464b830286429
mmmahiii <112865015+mmmahiii@users.noreply.github.com>
2022-09-12 12:05:39 +0100

    Update testing.py

--- 4 - Wind Chill/testing.py
+++ 4 - Wind Chill/testing.py
@@ -1,1 +1,13 @@
-f
+import unittest
+
+from wind_chill import*
+
+
+class MyFirstTests(unittest.TestCase):
+
+    def test_convertTemp(self):
+        self.assertEqual(convertTemp(10,15), -6.5895344209562525)
+
+
+if __name__ == '__main__':
+    unittest.main()
