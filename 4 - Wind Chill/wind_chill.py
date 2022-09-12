14596978bcfaacdbe5cc83d2fe772e1575c75635
mmmahiii <112865015+mmmahiii@users.noreply.github.com>
2022-09-12 12:06:28 +0100

    Update wind_chill.py

--- 4 - Wind Chill/wind_chill.py
+++ 4 - Wind Chill/wind_chill.py
@@ -1,1 +1,10 @@
-s
+def convertTemp(air_temp,air_speed):
+    tempIndex = 35.74 + 0.6215 * air_temp - 35.75 * air_speed**0.16 + 0.4275 * air_temp * air_speed**0.16
+    return tempIndex
+
+def setTemps():
+    print (f"Temperature of 10 and speed of 15 gives windchill of: {convertTemp(10,15)}")
+
+setTemps()
+
+
